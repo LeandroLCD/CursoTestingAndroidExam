8307dc5020d8b80828be7c8970b60355168de3a9
Leandro Colmenarez <leandrojcolmenarezd@gmail.com>
2026-07-08 19:24:16 -0400

    style: reformat OrderErrorDispatcher constructor
    - Add trailing comma and update parameter formatting in `OrderErrorDispatcher`

--- app/src/androidTest/java/com/aristidevs/cursotestingandroid/core/mockwebserver/OrderErrorDispatcher.kt
+++ app/src/androidTest/java/com/aristidevs/cursotestingandroid/core/mockwebserver/OrderErrorDispatcher.kt
@@ -4,7 +4,9 @@ import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
 
-class OrderErrorDispatcher(private val httpCode: Int = 500) : Dispatcher() {
+class OrderErrorDispatcher(
+    private val httpCode: Int = 500,
+) : Dispatcher() {
     override fun dispatch(request: RecordedRequest): MockResponse =
         MockResponse()
             .setResponseCode(httpCode)
